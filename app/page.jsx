06b6da35f37eef9a3d2e3e2ece82845ab3c88ff2
Alexandre Angrignon <alex040892@gmail.com>
2024-11-26 20:57:24 +1300

    metadata and assets

--- app/page.jsx
+++ app/page.jsx
@@ -1,5 +1,9 @@
 import React from 'react';
-
+export const metadata = {
+  title: 'Property Pulse',
+  keywords: 'rental, property, real estate',
+  description: 'find the perfect rental property',
+};
 const HomePage = () => {
   return <div className='text-2xl'>HomePage</div>;
 };
