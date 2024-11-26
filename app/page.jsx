6bf54bc27684fd61ee063b08880999aa6300dc71
Alexandre Angrignon <alex040892@gmail.com>
2024-11-27 11:40:55 +1300

    File Based Routing

--- app/page.jsx
+++ app/page.jsx
@@ -1,11 +1,13 @@
+import Link from 'next/link';
 import React from 'react';
-export const metadata = {
-  title: 'Property Pulse',
-  keywords: 'rental, property, real estate',
-  description: 'find the perfect rental property',
-};
+
 const HomePage = () => {
-  return <div className='text-2xl'>HomePage</div>;
+  return (
+    <div>
+      <h1 className='text-3xl'>Welcome</h1>
+      <Link href='/properties'>Go to Properties</Link>
+    </div>
+  );
 };
 
 export default HomePage;
---
+++ app/properties/add/page.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const AddPropertyPage = () => {
+  return <div>Add Propety Page</div>;
+};
+
+export default AddPropertyPage;
