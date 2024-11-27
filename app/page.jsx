b976f535f30f29e86e3ef54ce1aa18d678df6510
Alexandre Angrignon <alex040892@gmail.com>
2024-11-28 12:03:19 +1300

    Database Connection With Mongoose

--- app/page.jsx
+++ app/page.jsx
@@ -4,9 +4,10 @@ import InfoBox from '@/components/InfoBox';
 import InfoBoxes from '@/components/InfoBoxes';
 import Link from 'next/link';
 import React from 'react';
-
+// import connectDB from '@/config/database';
 const HomePage = () => {
   // console.log(process.env.MONGODB_URI);
+  // connectDB();
   return (
     <>
       <Hero />
