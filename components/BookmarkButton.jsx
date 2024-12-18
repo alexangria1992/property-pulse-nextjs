98dead2e6bfef224f91007fe6f5f0ad6e67273f5
Alexandre Angrignon <alex040892@gmail.com>
2024-12-18 20:54:54 +1300

    Bookmark Action

--- components/BookmarkButton.jsx
+++ components/BookmarkButton.jsx
@@ -1,6 +1,8 @@
+'use client';
 import React from 'react';
-import { FaBook, FaBookmark } from 'react-icons/fa';
-const BookmarkButton = () => {
+import { FaBookmark } from 'react-icons/fa';
+import { FaBookmark } from 'react-icons/fa';
+const BookmarkButton = ({ property }) => {
   return (
     <button className='bg-blue-500 hover:bg-blue-600 text-white font-bold w-full py-2 px-4 rounded-full flex items-center justify-center'>
       <FaBookmark className='fas fa-bookmark mr-2' /> Property
