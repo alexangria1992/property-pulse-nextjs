ba7950cee371dcef3df5a90639698bf65f61aaa3
Alexandre Angrignon <alex040892@gmail.com>
2024-12-18 22:04:41 +1300

    Check Bookmark Status

---
+++ app/actions/checkBookmarkStatus.js
@@ -0,0 +1,28 @@
+'use server';
+
+import connectDB from '@/config/database';
+import User from '@/models/User';
+import { getSessionUser } from '@/utils/getSessionUser';
+import { revalidatePath } from 'next/cache';
+
+async function checkBookmarkStatus(propertyId) {
+  await connectDB();
+
+  const sessionUser = await getSessionUser();
+
+  if (!sessionUser || !sessionUser.userId) {
+    return { error: 'User ID is required' };
+  }
+
+  const { userId } = sessionUser;
+
+  // Find user in database
+  const user = await User.findById(userId);
+
+  // Check if property is bookmarked
+  let isBookmarked = user.bookmarks.includes(propertyId);
+
+  return { isBookmarked };
+}
+
+export default checkBookmarkStatus;
