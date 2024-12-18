6621258f520103d61e09c681faa24b0fc0fab34c
Alexandre Angrignon <alex040892@gmail.com>
2024-12-18 14:27:17 +1300

    Mapbopx Map Display

--- components/PropertyMap.jsx
+++ components/PropertyMap.jsx
@@ -1,11 +1,15 @@
 'use client';
+import Image from 'next/image';
 import { useEffect, useState } from 'react';
+import Map, { Marker } from 'react-map-gl';
 import { setDefaults, fromAddress } from 'react-geocode';
-
+import pin from '@/assets/images/pin.svg';
+import Spinner from './Spinner';
+import 'mapbox-gl/dist/mapbox-gl.css';
 const PropertyMap = ({ property }) => {
   const [lat, setLat] = useState(null);
   const [lng, setLng] = useState(null);
-  const [viewport, setVewport] = useState({
+  const [viewport, setViewport] = useState({
     latitude: 0,
     longitude: 0,
     zoom: 12,
@@ -53,11 +57,29 @@ const PropertyMap = ({ property }) => {
     fetchCoords();
   }, []);
 
-  if (loading) return <h3>Loading...</h3>;
+  if (loading) return <Spinner />;
   if (geocodeError)
     return <div className='text-xl'>No Location data found</div>;
 
-  return <div>PropertyMap</div>;
+  return (
+    !loading && (
+      <Map
+        mapboxAccessToken={process.env.NEXT_PUBLIC_MAPBOX_TOKEN}
+        mapLib={import('mapbox-gl')}
+        initialViewState={{
+          longitude: lng,
+          latitude: lat,
+          zoom: 15,
+        }}
+        style={{ width: '100%', height: 500 }}
+        mapStyle='mapbox://styles/mapbox/streets-v9'
+      >
+        <Marker longitude={lng} latitude={lat} anchor='bottom'>
+          <Image src={pin} alt='location' width={40} height={40} />
+        </Marker>
+      </Map>
+    )
+  );
 };
 
 export default PropertyMap;
