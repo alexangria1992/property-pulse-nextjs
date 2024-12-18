6fcde49c534f39ef5ac923a53e5396d65e00b139
Alexandre Angrignon <alex040892@gmail.com>
2024-12-18 20:40:03 +1300

    Property Page Sidebar

--- app/properties/[id]/page.jsx
+++ app/properties/[id]/page.jsx
@@ -5,7 +5,10 @@ import connectDB from '@/config/database';
 import Property from '@/models/Property';
 import { convertToSerializableObject } from '@/utils/convertToObject';
 import Link from 'next/link';
+import ShareButtons from '@/components/ShareButtons';
 import { FaArrowLeft } from 'react-icons/fa';
+import BookmarkButton from '@/components/BookmarkButton';
+import PropertyContactForm from '@/components/PropertyContactForm';
 const PropertyPage = async ({ params }) => {
   await connectDB();
   const propertyDoc = await Property.findById(params.id).lean();
@@ -37,6 +40,11 @@ const PropertyPage = async ({ params }) => {
         <div className='container m-auto py-10 px-6'>
           <div className='grid grid-cols-1 md:grid-cols-70/30 w-full gap-6'>
             <PropertyDetails property={property} />
+            <aside className='space-y-4'>
+              <BookmarkButton />
+              <ShareButtons property={property} />
+              <PropertyContactForm property={property} />
+            </aside>
           </div>
         </div>
       </section>
