ceea9d816b18fe4a864882cc58a8282c9c968cdc
Alexandre Angrignon <alex040892@gmail.com>
2024-12-19 13:37:48 +1300

    Saved Properties Page

---
+++ app/properties/saved/page.jsx
@@ -0,0 +1,33 @@
+import PropertyCard from '@/components/PropertyCard';
+import React from 'react';
+import connectDB from '@/config/database';
+import User from '../../../models/User';
+import { getSessionUser } from '../../../utils/getSessionUser';
+
+const SavedPropertiesPage = async () => {
+  const { userId } = await getSessionUser();
+
+  const { bookmarks } = await User.findById(userId).populate('bookmarks');
+
+  console.log(bookmarks);
+
+  console.log(userId);
+  return (
+    <section className='px-4 py-6'>
+      <div className='container m-auto px-4 py-6 lg:container'>
+        <h1 className='text-2xl mb-4'>Saved Properties</h1>
+        {bookmarks.length === 0 ? (
+          <p>No Saved Properties</p>
+        ) : (
+          <div className='grid grid-cols-1 md:grid-cols-3 gap-6'>
+            {bookmarks.map((property) => (
+              <PropertyCard key={property._id} property={property} />
+            ))}
+          </div>
+        )}
+      </div>
+    </section>
+  );
+};
+
+export default SavedPropertiesPage;
